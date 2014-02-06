98d09a3b382825c6bb841ab5b6432fc741d5bbf8
duguying <duguying2008@gmail.com>
2014-02-06 14:48:12 +0800

    login page

--- src/com/rex/yuol/Welcome.java
+++ src/com/rex/yuol/Welcome.java
@@ -15,7 +15,7 @@ public class Welcome extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.welcome, menu);
+//		getMenuInflater().inflate(R.menu.welcome, menu);
 		return true;
 	}
 
