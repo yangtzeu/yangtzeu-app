ed41564ef0371eb652e9d9ff77f57c70c480c87c
duguying <duguying2008@gmail.com>
2014-02-08 13:35:52 +0800

    add json-lib and urls config file

---
+++ src/com/rex/yuol/config/Urls.java
@@ -0,0 +1,29 @@
+package com.rex.yuol.config;
+
+public class Urls {
+
+	// 教务处
+	public static String jwc_home = "http://jwc.yangtzeu.edu.cn:8080/";
+	public static String jwc_login_page = jwc_home + "login.aspx";
+	public static String jwc_login_response = jwc_home + "login.aspx";
+	public static String jwc_cjcx_page = jwc_home + "cjcx.aspx";
+	public static String jwc_cjcx_response = jwc_home + "cjcx.aspx";
+	public static String jwc_search_student_page = jwc_home + "student.aspx";
+	public static String jwc_search_student_response = jwc_home
+			+ "student.aspx";
+	public static String jwc_search_class_student_page = jwc_home
+			+ "student.aspx";
+	public static String jwc_search_class_student_response = jwc_home
+			+ "student.aspx";
+
+	// 长大首页
+	public static String yz_home = "http://www.yangtzeu.edu.cn/";
+	public static String yz_news_home = "http://news.yangtzeu.edu.cn/";
+
+	// 长大在线
+	public static String yuol_home = "http://online.yangtzeu.edu.cn/";
+
+	// api
+	public static String api_home = "http://yuol.duapp.com/";
+
+}
