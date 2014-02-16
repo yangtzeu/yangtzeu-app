a94d654cedec7210411fd4f04eca74aa72442bbd
duguying <duguying2008@gmail.com>
2014-02-16 13:20:45 +0800

    title icons and add some other basic pages

---
+++ src/com/rex/yuol/Library.java
@@ -0,0 +1,22 @@
+package com.rex.yuol;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Library extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.library);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.library, menu);
+		return true;
+	}
+
+}
