a240b0e69db1f103b074b0f5f9b03aa72346f210
duguying <duguying2008@gmail.com>
2014-02-23 22:32:18 +0800

    news catalog list

--- src/com/rex/yuol/News.java
+++ src/com/rex/yuol/News.java
@@ -1,9 +1,16 @@
 package com.rex.yuol;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class News extends Activity {
 
@@ -11,6 +18,24 @@ public class News extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.news);
+
+		String[] news_cata_items = this.getResources().getStringArray(
+				R.array.news);
+
+		List<Map<String, Object>> contents = new ArrayList<Map<String, Object>>();
+		for (int i = 0; i < news_cata_items.length; i++) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("list_item_title", news_cata_items[i]);
+			map.put("list_item_title_notice", "");
+			contents.add(map);
+		}
+		SimpleAdapter adapter = new SimpleAdapter(this, contents,
+				R.layout.news_cata_item,
+				new String[] { "list_item_title", "list_item_title_notice" }, new int[] {
+						R.id.list_item_title, R.id.list_item_title_notice });
+
+		ListView listview = (ListView) findViewById(R.id.news_cata_list);
+		listview.setAdapter(adapter);
 	}
 
 	@Override
