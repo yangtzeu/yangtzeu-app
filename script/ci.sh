6962b92400730ad553528bfb7ce44d1bdad123e1
Rex Lee(李俊) <duguying2008@gmail.com>
2014-05-23 17:17:28 +0800

    ci script

--- script/ci.sh
+++ script/ci.sh
@@ -2,15 +2,15 @@
 
 # download the latest android sdk and unzip
 wget http://dl.google.com/android/android-sdk_r21-linux.tgz
-tar -zxf android-sdk_r21-linux.tgz
+tar -zxf android-sdk_r21-linux.tgz > /dev/null
 
 export ANDROID_SDK_HOME=${PWD}/android-sdk-linux
 export PATH=${PATH}:${ANDROID_SDK_HOME}/tools:${ANDROID_SDK_HOME}/platform-tools
 
-sudo apt-get update -qq
-sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch
+sudo apt-get update -qq > /dev/null
+sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch > /dev/null
 # update sdk, get android api from net
-android update sdk --filter platform-tools,android-8,extra-android-support,$ANDROID_SDKS --no-ui --force
+android update sdk --filter platform-tools,android-8,extra-android-support,$ANDROID_SDKS --no-ui --force > /dev/null
 
 export ANDROID_API_DIR=${ANDROID_SDK_HOME}/platforms/${ANDROID_SDKS}
 export ZIPALIGN=`find ${ANDROID_SDK_HOME}/tools/ -name 'zipalign'`
@@ -18,8 +18,8 @@ export AAPT=`find ${ANDROID_SDK_HOME}/platform-tools/ -name 'aapt'`
 export AIDL=`find ${ANDROID_SDK_HOME}/platform-tools/ -name 'aidl'`
 export DX=`find ${ANDROID_SDK_HOME}/platform-tools/ -name 'dx'`
 
-sudo apt-get install tree
-tree ${ANDROID_SDK_HOME}
+# sudo apt-get install tree > /dev/null
+ls -al ${ANDROID_SDK_HOME}
 echo ANDROID_SDK_HOME\:${ANDROID_SDK_HOME}
 echo ZIPALIGN\:${ZIPALIGN}
 echo AAPT\:${AAPT}
